6fd963f195beb45f1b8130045ec78760c511b145
Samat <samat1989@mail.ru>
2017-10-18 11:18:59 +0300

    correcting homepage

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
-  // весь ваш код
+$(document).ready(function() {
+  // // script for Burger and Main Menu
+  // $('#burger').on('click', function(){
+
+  // $('#nav').toggleClass('main-nav--close');
+
+  // $('#burger>span').toggleClass('burger__lines--close cross');
+
 });
