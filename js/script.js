55799162361da32795c5fe75b66c49ec29cf17d5
Samat <samat1989@mail.ru>
2017-10-27 17:14:03 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // // script for Burger and Main Menu
+  // $('#burger').on('click', function(){
+
+  // $('#nav').toggleClass('main-nav--close');
+
+  // $('#burger>span').toggleClass('burger__lines--close cross');
+
+});
