959038b3cb90fb0299515fbf48b20d1b8804f189
Samat <samat1989@mail.ru>
2017-11-19 22:45:49 +0300

    starter writing scripts

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,65 @@
 $(document).ready(function() {
 
+    $(".navigation-bar_menu-item").hover(function(event) {
+        event.preventDefault();
+        $(this).toggleClass("_active");
+
+        var menuItem = document.querySelector('.navigation-bar_menu-item');
+        if(menuItem.classList.contains('_active')) {
+            document.querySelector('.navigation-bar-secondary').style.display = ('flex');
+        }
+    });
+
+    $('.homepage').hover(function() {
+       $(".navigation-bar_menu-item ._active").removeClass('_active');
+        if(!menuItem.classList.contains('_active')) {
+            document.querySelector('.navigation-bar-secondary').style.display = ('none');
+        }
+    });
+
+
+
 });
 
 
-document.querySelector('.navigation-bar_menu-item').addEventListener('click', function(e) {
-    e.preventDefault();
+// document.querySelector('.navigation-bar_menu-item').addEventListener('mouseover', function(e) {
+//     e.preventDefault();
 
-    var menuItem = document.querySelector('.navigation-bar_menu-item');
+//     var menuItem = document.querySelector('.navigation-bar_menu-item');
 
-        menuItem.classList.toggle('_active');
+//         menuItem.classList.add('_active');
 
-        if(menuItem.classList.contains('_active')) {
-            document.querySelector('.navigation-bar-secondary').style.display = ('flex');
-        } else {
-             document.querySelector('.navigation-bar-secondary').style.display = ('none');
-        }
-})
+//         if(menuItem.classList.contains('_active')) {
+//             document.querySelector('.navigation-bar-secondary').style.display = ('flex');
+//         } else {
+//              document.querySelector('.navigation-bar-secondary').style.display = ('none');
+//         }
+// })
+
+// document.querySelector('.navigation-bar-secondary').addEventListener('mouseover', function(e) {
+//     e.preventDefault();
+
+//       var menuItem = document.querySelector('.navigation-bar_menu-item');
+
+
+//         if(menuItem.classList.contains('_active')) {
+//             document.querySelector('.navigation-bar-secondary').style.display = ('flex');
+//         } else {
+//              document.querySelector('.navigation-bar-secondary').style.display = ('none');
+//         }
+
+// });
+
+// document.querySelector('.navigation-bar_menu-item').addEventListener('mouseout', function(e) {
+//     e.preventDefault();
+
+//     var menuItem = document.querySelector('.navigation-bar_menu-item');
+
+//         menuItem.classList.remove('_active');
+
+//         if(menuItem.classList.contains('_active')) {
+//             document.querySelector('.navigation-bar-secondary').style.display = ('flex');
+//         } else {
+//              document.querySelector('.navigation-bar-secondary').style.display = ('none');
+//         }
+// })
