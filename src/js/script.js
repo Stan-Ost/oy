08f7929631200e1038ff13c9f35ea225140c1fa2
Samat <samat1989@mail.ru>
2017-11-26 00:36:42 +0300

    added accordion in footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,65 +1,46 @@
 $(document).ready(function() {
 
-    $(".navigation-bar_menu-item").hover(function(event) {
-        event.preventDefault();
-        $(this).toggleClass("_active");
+    // $(".navigation-bar_menu-item").hover(function(event) {
+    //     event.preventDefault();
+    //     $(this).toggleClass("_active");
 
-        var menuItem = document.querySelector('.navigation-bar_menu-item');
-        if(menuItem.classList.contains('_active')) {
-            document.querySelector('.navigation-bar-secondary').style.display = ('flex');
-        }
-    });
+    //     var menuItem = document.querySelector('.navigation-bar_menu-item');
+    //     if(menuItem.classList.contains('_active')) {
+    //         document.querySelector('.navigation-bar-secondary').style.display = ('flex');
+    //     }
+    // });
 
-    $('.homepage').hover(function() {
-       $(".navigation-bar_menu-item ._active").removeClass('_active');
-        if(!menuItem.classList.contains('_active')) {
-            document.querySelector('.navigation-bar-secondary').style.display = ('none');
-        }
-    });
-
+    // $('.homepage').hover(function() {
+    //    $(".navigation-bar_menu-item ._active").removeClass('_active');
+    //     if(!menuItem.classList.contains('_active')) {
+    //         document.querySelector('.navigation-bar-secondary').style.display = ('none');
+    //     }
+    // });
 
 
 });
 
 
-// document.querySelector('.navigation-bar_menu-item').addEventListener('mouseover', function(e) {
-//     e.preventDefault();
-
-//     var menuItem = document.querySelector('.navigation-bar_menu-item');
-
-//         menuItem.classList.add('_active');
-
-//         if(menuItem.classList.contains('_active')) {
-//             document.querySelector('.navigation-bar-secondary').style.display = ('flex');
-//         } else {
-//              document.querySelector('.navigation-bar-secondary').style.display = ('none');
-//         }
-// })
-
-// document.querySelector('.navigation-bar-secondary').addEventListener('mouseover', function(e) {
-//     e.preventDefault();
-
-//       var menuItem = document.querySelector('.navigation-bar_menu-item');
 
 
-//         if(menuItem.classList.contains('_active')) {
-//             document.querySelector('.navigation-bar-secondary').style.display = ('flex');
-//         } else {
-//              document.querySelector('.navigation-bar-secondary').style.display = ('none');
-//         }
 
-// });
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-// document.querySelector('.navigation-bar_menu-item').addEventListener('mouseout', function(e) {
-//     e.preventDefault();
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("_active");
 
-//     var menuItem = document.querySelector('.navigation-bar_menu-item');
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "inline-block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "inline-block";
+        }
+    }
+}
 
-//         menuItem.classList.remove('_active');
 
-//         if(menuItem.classList.contains('_active')) {
-//             document.querySelector('.navigation-bar-secondary').style.display = ('flex');
-//         } else {
-//              document.querySelector('.navigation-bar-secondary').style.display = ('none');
-//         }
-// })
