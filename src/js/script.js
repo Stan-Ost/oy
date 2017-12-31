680ea9aaa6e0f06f43bfed9292b8e87668cedcd3
Samat <samat1989@mail.ru>
2017-12-31 19:48:21 +0300

    sssss

--- src/js/script.js
+++ src/js/script.js
@@ -91,13 +91,16 @@ function openTab(evt, cityName) {
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace("_active", "");
+        tablinks[i].className = tablinks[i].className.replace(" _active", "");
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "inline-block";
-    evt.currentTarget.className += "_active";
+    evt.currentTarget.className += " _active";
 }
+
+document.getElementById("defaultOpen").click();
+
 
 
 
